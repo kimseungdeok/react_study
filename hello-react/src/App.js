9bd2b302db91ed9716066065a72556b8e6c66884
KimSeungDeok <tmdejr1117@gmail.com>
2022-02-08 11:50:08 +0900

    2.4.4

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,16 +3,8 @@ import './App.css';
 import { Fragment } from 'react';
 
 function App() {
-  const name = '리액트';
-  return (
-    <div>
-      {name === '리액트' ? (
-        <h1>리액트입니다.</h1>
-      ) : (
-        <h2>리액트가 아닙니다.</h2>
-      )}
-    </div>
-  );
+  const name = '뤼왝트';
+  return <div>{name === '리액트' ? (<h1>리액트입니다.</h1>) : null}</div>;
 }
 
 export default App;
