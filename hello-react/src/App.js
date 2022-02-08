bd164201878dc5f6bef21e5327d6f40a984eb667
KimSeungDeok <tmdejr1117@gmail.com>
2022-02-08 13:50:46 +0900

    2.4.6

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,8 +3,19 @@ import './App.css';
 import { Fragment } from 'react';
 
 function App() {
-  const name = undefined;
-  return <div>{name || '리액트'}</div>;
-}
+  const name = '리액트';
+
+  return <div 
+  style={{
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16 // 단위를 생략하면 px로 지정됩니다.
+  }}
+  >
+    {name}
+  </div>;
+};
 
 export default App;
