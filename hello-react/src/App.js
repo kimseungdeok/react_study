3a8800844bea20278e572f2e196c0ab227383d99
KimSeungDeok <tmdejr1117@gmail.com>
2022-02-08 13:01:23 +0900

    2.4.4 falsy

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import { Fragment } from 'react';
 
 function App() {
-  const name = '뤼왝트';
-  return <div>{name === '리액트' && <h1>리액트입니다.</h1>}</div>;
+  // const name = '뤼왝트';
+  // return <div>{name === '리액트' && <h1>리액트입니다.</h1>}</div>;
+  const number = 0;
+  return number && <div>내용</div>;
 }
 
 export default App;
