abb1b2b40229e6a35df1d7b061d27ca0d452bf7f
KimSeungDeok <tmdejr1117@gmail.com>
2022-02-08 14:58:57 +0900

    2장 완료

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -5,17 +5,7 @@ import { Fragment } from 'react';
 function App() {
   const name = '리액트';
 
-  return <div 
-  style={{
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: 16 // 단위를 생략하면 px로 지정됩니다.
-  }}
-  >
-    {name}
-  </div>;
+  return <div className='react'>{name}</div>
 };
 
 export default App;
--- jsx.js
+++ jsx.js
@@ -5,3 +5,20 @@ function App(){
         </div>
     );
 }
+
+function App(){
+    return React.createElement("div", null, "Hello", React.createElement("b", null, "react"));
+}
+
+import { Fragment } from 'react';
+
+function App() {
+    return (
+        <Fragment>
+            <h1>리액트 안녕!</h1>
+            <h2>잘 작동하니?</h2>
+        </Fragment>
+    );
+  }
+  
+  export default App;
