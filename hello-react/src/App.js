85812799f4e536968f28f0ec96fe305310fa91be
KimSeungDeok <tmdejr1117@gmail.com>
2022-02-10 23:22:37 +0900

    3.3.3 defaultProps

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import { Fragment } from 'react';
+import React from 'react';
 import MyComponent from './MyComponent';
-
+ 
 const App = () => {
   return <MyComponent />;
 };
-
+ 
 export default App;
