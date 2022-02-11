7736d511659d0cdfb9fc8a6dc6e8b47c56e26f47
KimSeungDeok <tmdejr1117@gmail.com>
2022-02-11 11:26:29 +0900

    3.4.1

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import MyComponent from './MyComponent';
+// import MyComponent from './MyComponent';
+import Counter from './Counter';
  
 const App = () => {
-  return <MyComponent name='React' favoriteNumber={3}>리액트</MyComponent>;
+  // return <MyComponent name='React' favoriteNumber={3}>리액트</MyComponent>;
+  return <Counter />;
 };
  
 export default App;
