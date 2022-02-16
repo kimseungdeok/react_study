30eba772a0062924cfcd8d44bce7ed8f0ab1f52a
KimSeungDeok <tmdejr1117@gmail.com>
2022-02-16 10:51:27 +0900

    6장 완료

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -5,7 +5,7 @@ const IterationSample = () => {
         { id: 1, text: '눈사람'},
         { id: 2, text: '얼음'},
         { id: 3, text: '눈'},
-        { id: 4, text: '바람'},
+        { id: 4, text: '바람'}
     ]);
     const [inputText, setInputText] = useState('');
     const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
@@ -18,17 +18,26 @@ const IterationSample = () => {
             text: inputText
         });
         setNextId(nextId + 1); // nextId 값에 1을 더해준다.
-        setNames(setNames); // names 값을 업데이트한다.
+        setNames(nextNames); // names 값을 업데이트한다.
         setInputText(''); // inputText를 비운다.
-    }
+    };
 
-    const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+    const onRemove = id => {
+        const nextNames = names.filter(name => name.id !== id);
+        setNames(nextNames);
+    };
+
+    const namesList = names.map(name => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+        {name.text}
+    </li>
+    ));
     return (
     <>
         <input value={inputText} onChange={onChange} />
         <button onClick={onClick}>추가</button>
-        <ul>{namesList}</ul>;
+        <ul>{namesList}</ul>
     </>
-    )
-}
+    );
+};
 export default IterationSample;
