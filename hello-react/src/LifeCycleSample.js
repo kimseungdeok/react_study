6b7ad118bdf826642b56ac676c7ddcb2c73dcd95
KimSeungDeok <tmdejr1117@gmail.com>
2022-02-18 20:32:54 +0900

    7장 끝

--- hello-react/src/LifeCycleSample.js
+++ hello-react/src/LifeCycleSample.js
@@ -53,21 +53,21 @@ class LifeCycleSample extends Component {
         console.log('render');
 
         const style = {
-            color: this.props.color
+            color : this.props.color
         };
 
         return(
             <div>
-                <h1 style={style} ref={ref => this.myRef=ref}>
-                    {this.state.number}
-                </h1>
-                <p>color: {this.state.color}</p>
-                <button onClick={this.handleClick}>
-                    더하기
-                </button>
-            </div>
-        )
+            {this.props.missing.value}
+            <h1 style={style} ref={ref => (this.myRef = ref)}>
+              {this.state.number}
+            </h1>
+            <p>color: {this.state.color}</p>
+            <button onClick={this.handleClick}>더하기</button>
+          </div>
+        );
+
     }
 }
 
-export default LifeCycleSample;
+export default LifeCycleSample
