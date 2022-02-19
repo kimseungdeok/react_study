a7901f57fda0e7a77118977908c5ff5efebdfdec
KimSeungDeok <tmdejr1117@gmail.com>
2022-02-19 15:57:10 +0900

    8.2.2

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -4,12 +4,8 @@ const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
     useEffect(() => {
-        console.log('렌더링이 완료되었습니다.');
-        console.log({
-            name,
-            nickname
-        });
-    });
+        console.log(name);
+    }, [name]);
 
     const onChangeName = e => {
         setName(e.target.value);
