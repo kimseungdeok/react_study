921130d76c78e464042dc482c9837a819a174eb4
KimSeungDeok <tmdejr1117@gmail.com>
2022-02-25 00:09:38 +0900

    9.4.2

--- styling-react/src/StyledComponent.js
+++ styling-react/src/StyledComponent.js
@@ -22,13 +22,14 @@ const Button = styled.button`
     /* & 문자를 사용하여 Sass처럼 자기 자신 선택 가능 */
     &:hover {
         background: rgba(255, 255, 255, 0.9); 
+    }
 
     /* 다음 코드는 inverted 값이 true일 때 특정 스타일을 부여해 준다.*/
     ${props => 
     props.inverted && 
     css`
         background: none;
-        border: 2px solid white
+        border: 2px solid white;
         color: white;
         &:hover {
             background: white;
