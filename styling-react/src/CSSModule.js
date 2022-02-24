cba0690c6f9d071e006c194ecb0d01a4f9e82287
KimSeungDeok <tmdejr1117@gmail.com>
2022-02-24 23:19:19 +0900

    9.4.0

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './CSSModule.module.css'
+import styles from './CSSModule.module.scss'
 
 const CSSModule = () => {
     return (
@@ -9,3 +9,4 @@ const CSSModule = () => {
     );
 };
 export default CSSModule;
+
