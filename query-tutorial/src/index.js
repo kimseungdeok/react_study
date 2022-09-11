be6da89381b39296d1362c705db53ba20004b425
kimseungdeok <tmdejr1117@gmail.com>
2022-09-11 23:11:49 +0900

    finish : 리액트 쿼리 학습

---
+++ query-tutorial/src/index.js
@@ -0,0 +1,19 @@
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      {/* devtools */}
+      <ReactQueryDevtools initialIsOpen={true} />
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
